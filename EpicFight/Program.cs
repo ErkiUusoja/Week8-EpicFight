be43795219d5684b8a234cdd71d911824a413db2
Opilane TTHK <opilane@tthk.ee>
2021-10-26 10:09:18 +0300

    fight

--- EpicFight/Program.cs
+++ EpicFight/Program.cs
@@ -8,13 +8,57 @@ namespace EpicFight
         {
             string hero = PickHero();
             string villain = PickVillain();
+            int heroHP = GenerateHP(hero);
+            int villainHP = GenerateHP(villain);
             Console.WriteLine($"{hero} will fight {villain}.");
 
             string heroWeapon = PickWeapon();
             string VillainWeapon = PickWeapon();
             Console.WriteLine($"{hero} picked {heroWeapon}. {villain} picked {VillainWeapon}");
 
+            while(heroHP > 0 && villainHP > 0)
+            {
+                heroHP = heroHP - Hit(hero, villain, heroWeapon);
+                villainHP = villainHP - Hit(hero, villain, heroWeapon);
+            }
+
+            if(heroHP <= 0)
+            {
+                Console.WriteLine("Dark side wins");                                   
+            }
+            else
+            {
+                Console.WriteLine($"{hero} saves the day!");
+            }
         }
+
+        private static int GenerateHP(string someName)
+        {
+            Random rnd = new Random();
+            return rnd.Next(someName.Length, someName.Length + 10);
+
+
+        }
+
+        private static int Hit(string characterOne, string characterTwo, string someWeapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, someWeapon.Length / 2);
+
+            Console.WriteLine($"{characterOne} hit {strike}.");
+            if(strike == someWeapon.Length / 2)
+            {
+                Console.WriteLine($"Awesome! {characterOne} made a critical hit!");
+            }
+            else if (strike == 0)
+            {
+                Console.WriteLine($"{characterTwo} dodged the attack!");
+            }
+
+            return strike;
+
+        }
+
 
         private static string PickHero()
         {
